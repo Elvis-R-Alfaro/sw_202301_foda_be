da9b90a585c59e89aca1acf72e606bd58b0f8a65
Elvis Alfaro <ealfaro810@gmail.com>
2023-01-30 12:41:08 -0600

    26/01/2023

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,29 @@
 import express from 'express';
 const router  = express.Router();
+// REST API
+// Internet => HTTP => REST API => DB
+// SOAP XML wsdl
+// {} -> JSON
+// [] -> JSON
+// { key: value }
+// valor: texto, numero, booleano, array[valores], objeto{key:valor
+
+// RST stateless, resource unique presentation
+// CRUD Create, Read, Update, Delete
+// HTTP   POST,  GET,   PUT,   DELETE
 
 router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
- });
+});
+
+router.get('/version', (_req, res) =>{
+  const version: string = '1.0.0';
+  const jsonResp = {
+    "name" : "FODA Be",
+    "version" : version
+  };
+  // string, number, boolean, types, interfaces, classes, enumerators
+  res.json(jsonResp);
+});
 
 export default router;
